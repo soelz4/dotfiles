976efa0fe597084db63938d604d30d96c9849fef
Soheil <92805087+soelz4@users.noreply.github.com>
2024-03-04 22:26:55 +0330

    Add files via upload

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,9 +6,9 @@ return {
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.diagnostics.eslint,
+					--null_ls.builtins.diagnostics.eslint,
 					null_ls.builtins.completion.spell,
-					null_ls.builtins.formatting.rustfmt,
+					--null_ls.builtins.formatting.rustfmt,
 					null_ls.builtins.formatting.gofumpt,
 					null_ls.builtins.formatting.goimports_reviser,
 					null_ls.builtins.formatting.golines,

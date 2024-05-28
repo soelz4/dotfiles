8c6ea157dcf1095ee5a5fbfd68d5986b75e22646
soelz4-artix-runit <soheilzolfaghari78@gmail.com>
2024-05-28 11:06:14 +0330

    telescope.lua Updated

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -23,6 +23,7 @@ return {
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fr", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+			vim.keymap.set("n", "<leader>km", builtin.keymaps, {})
 		end,
 	},
 	{

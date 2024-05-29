8ff5acf82195d1e3ce3cc619ab1227fc3ad73e08
soelz4-artix-runit <soheilzolfaghari78@gmail.com>
2024-05-29 12:33:54 +0330

    ui.lua Updated

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -254,8 +254,10 @@ return {
 		"echasnovski/mini.indentscope",
 		version = false,
 		opts = {
+			-- symbol = '╎',
 			-- symbol = "▏",
-			symbol = "│",
+			-- symbol = "│",
+			symbol = "|",
 			options = { try_as_border = true },
 		},
 		config = function(_, opts)
